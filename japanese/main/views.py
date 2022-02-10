e73484a4ec2efd8640f2353ac76177925580d9d5
Susoroff <www.grandtop@gmail.com>
2022-02-10 10:46:35 +0200

    first working app

--- japanese/main/views.py
+++ japanese/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def main_menu(req):
+    slovo = [x for x in range(1,5)]
+    digit = [y for y in range(5,1,-1)]
+
+    tmp = list(zip(slovo , digit))
+    print(tmp)
+    firs = [1,2,3]
+    contex = {'first':firs,'tmp':tmp}
+
+    return render(req,'index.html',contex)
